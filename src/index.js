fad2fe293f79f4f289c80597e8067958160117e5
khushbu-bamroliya <80873905+khushbu-bamroliya@users.noreply.github.com>
2022-09-27 16:18:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/// database table name : test - users
+/// in server - npm start
+/// in react_crud project run - npm start
